00f1656f444df21b2243e4ed12b7c7accc2e60d1
Shivendra <shivendra@trinkerr.com>
2024-09-01 19:49:40 +0530

    Feat: added time column in csv

--- Backtesting/backtest.py
+++ Backtesting/backtest.py
@@ -15,35 +15,52 @@ def timeToIndex(time_input, start_time='09:15'):
         raise ValueError("Out of range time")
     return index
 
+def indexToTime(index, base_time='09:15'):
+    if index == -1:
+        return "N/A"
+    base_time_obj = datetime.strptime(base_time, "%H:%M")
+    time_delta = timedelta(minutes=index)
+    newTime = base_time_obj + time_delta
+    # Format the new time as a string in "HH:MM" format
+    return newTime.strftime("%H:%M")
+
 def calDayHigh(df, column_name, end_index):
     dayHigh = float('-inf')
+    dayHighIndex = -1
     for i in range(end_index + 1):
         currHigh = df.loc[i, column_name]
         if currHigh > dayHigh:
             dayHigh = currHigh
-    return dayHigh
+            dayHighIndex = i
+    return dayHigh, indexToTime(dayHighIndex)
 
 def calDayLow(df, column_name, end_index):
     dayLow = float('inf')
+    dayLowIndex = -1
     for i in range(end_index + 1):
         currLow = df.loc[i, column_name]
         if currLow < dayLow:
             dayLow = currLow
-    return dayLow
+            dayLowIndex = i
+    return dayLow, indexToTime(dayLowIndex)
 
 def calHighBreak(df, column_name, start_index, dayHigh):
+    highBreakIndex = -1
     for i in range(start_index+1,375):
         currHigh = df.loc[i,column_name]
         if currHigh > dayHigh:
-            return True
-    return False
+            highBreakIndex = i
+            return True , indexToTime(highBreakIndex)
+    return False , indexToTime(highBreakIndex)
             
 def calLowBreak(df,column_name, start_index, dayLow):
+    lowBreakIndex = -1
     for i in range(start_index+1,375):
         currLow = df.loc[i,column_name]
         if currLow < dayLow:
-            return True
-    return False
+            lowBreakIndex = i
+            return True, indexToTime(lowBreakIndex)
+    return False, indexToTime(lowBreakIndex)
 
 def comesBackToDayHigh(df, column_name, start_index, dayHigh, isHighBreak):
     if isHighBreak:
@@ -63,39 +80,47 @@ def comesBackToDayLow(df, column_name, start_index, dayLow, isLowBreak):
     
 def pointsToHighTarget(df, column_name, start_index, dayHigh, isDayHighBreak): #column_name = high
     target = dayHigh
+    targetHighIndex = -1
     if isDayHighBreak:
         for i in range(start_index+1, 375):
             currHigh = df.loc[i,column_name]
             if target <= currHigh:
                 target = currHigh
-    return target-dayHigh
+                targetHighIndex = i
+    return target-dayHigh, indexToTime(targetHighIndex) #it returns a tuple with points diff, time
 
 def pointsToHighSL(df, column_name, start_index, dayHigh, isDayHighBreak): #column_name = low
     stop_loss = dayHigh
+    highSLIndex = -1
     if isDayHighBreak:
         for i in range(start_index+1, 375):
             currLow = df.loc[i,column_name]
             if stop_loss >= currLow:
                 stop_loss = currLow
-    return stop_loss-dayHigh
+                highSLIndex = i
+    return stop_loss-dayHigh, indexToTime(highSLIndex) #it returns a tuple with points diff, time
 
 def pointsToLowTarget(df, column_name, start_index, dayLow, isDayLowBreak): #column_name = low
     target = dayLow
+    lowTargetIndex = -1
     if isDayLowBreak:
         for i in range(start_index+1, 375):
             currLow = df.loc[i,column_name]
             if target >= currLow:
                 target = currLow
-    return dayLow-target
+                lowTargetIndex = i
+    return dayLow-target, indexToTime(lowTargetIndex) #it returns a tuple with points diff, time
 
 def pointsToLowSL(df, column_name, start_index, dayLow, isDayLowBreak): #column_name = high
     stop_loss = dayLow
+    lowSLIndex = -1
     if isDayLowBreak:
         for i in range(start_index+1, 375):
             currHigh = df.loc[i,column_name]
             if currHigh >= stop_loss:
                 stop_loss = currHigh
-    return dayLow-stop_loss
+                lowSLIndex = i
+    return dayLow-stop_loss, indexToTime(lowSLIndex) #it returns a tuple with points diff, time
          
 def df_filter(df, columns):
     for column in columns:
@@ -106,7 +131,9 @@ def process_csv(csv_file_path, output_csv_path, index, time_input):
     chunk_size = 375
     column_names = ['Instrument', 'Date', 'Time', 'Open', 'High', 'Low', 'Close']
     day_data_chunk = pd.read_csv(csv_file_path, skiprows=1, chunksize=chunk_size, header=None)
-    new_df = pd.DataFrame(columns=['Date', 'Time', 'DayHigh', 'DayLow', 'IsDayHighBreak', 'IsDayLowBreak','ComesBackToDayHigh', 'ComesBackToDayLow', 'TargetHigh', 'points_from_target_high', 'percentChangeTargetHigh','SLHigh', 'points_from_SL_High', 'percentageChangeSLHigh' ,'TargetLow', 'points_from_Target_Low', 'percentageChangeTargetLow','SLLow', 'points_from_SL_low','percentageChangeSLLow', 'Closing', 'ClosingPointDiffForHigh', 'ClosingPointDiffForLow', 'noBreak'])
+    new_df = pd.DataFrame(
+        columns=['Date', 'Time', 'DayHigh', 'DayHighTime', 'DayLow', 'DayLowTime', 'IsDayHighBreak', 'DayHighBreakTime' ,'IsDayLowBreak', 'DayLowBreakTime', 'ComesBackToDayHigh', 'ComesBackToDayLow', 'TargetHigh', 'TargetHighTime', 'points_from_target_high', 'percentChangeTargetHigh', 'SLHigh', 'SLHighTime', 'points_from_SL_High', 'percentageChangeSLHigh', 'TargetLow', 'TargetLowTime', 'points_from_Target_Low', 'percentageChangeTargetLow', 'SLLow', 'SLLowTime', 'points_from_SL_low', 'percentageChangeSLLow', 'Closing', 'ClosingPointDiffForHigh', 'ClosingPointDiffForLow', 'noBreak']
+    )
 
     for day in day_data_chunk:
         if not day.empty:
@@ -120,19 +147,19 @@ def process_csv(csv_file_path, output_csv_path, index, time_input):
             date = day['Date'].iloc[0].astype(float).astype(int).astype(str)
             closing = day['Close'].iloc[373].astype(float)
             
-            dayHigh = calDayHigh(day, 'High', index)
-            isDayHighBreak = calHighBreak(day,'High',index,dayHigh)
+            dayHigh, dayHighTime = calDayHigh(day, 'High', index)
+            isDayHighBreak, dayHighBreakTime = calHighBreak(day,'High',index,dayHigh)
             
-            dayLow = calDayLow(day, 'Low', index)
-            isDayLowBreak = calLowBreak(day,'Low',index, dayLow)
+            dayLow, dayLowTime= calDayLow(day, 'Low', index)
+            isDayLowBreak, dayLowBreakTime = calLowBreak(day,'Low',index, dayLow)
             
             isComesBackToDayHigh = comesBackToDayHigh(day,'Low',index,dayHigh,isDayHighBreak)
             isComesBackToDayLow = comesBackToDayHigh(day,'High',index,dayLow,isDayLowBreak)
             
-            diff_from_target_high = pointsToHighTarget(day,'High', index, dayHigh, isDayHighBreak)
-            diff_from_sl_high = pointsToHighSL(day,'Low', index, dayHigh, isDayHighBreak)
-            diff_from_target_low = pointsToLowTarget(day,'Low', index, dayLow, isDayLowBreak)
-            diff_from_sl_low = pointsToLowSL(day,'High', index, dayLow, isDayLowBreak)
+            diff_from_target_high, target_high_time = pointsToHighTarget(day,'High', index, dayHigh, isDayHighBreak)
+            diff_from_sl_high, sl_high_time = pointsToHighSL(day,'Low', index, dayHigh, isDayHighBreak)
+            diff_from_target_low, tartget_low_time = pointsToLowTarget(day,'Low', index, dayLow, isDayLowBreak)
+            diff_from_sl_low , sl_low_time = pointsToLowSL(day,'High', index, dayLow, isDayLowBreak)
             
             target_high = 0
             sl_high = 0
@@ -154,7 +181,7 @@ def process_csv(csv_file_path, output_csv_path, index, time_input):
             target_low_percent = round((diff_from_target_low / dayLow) * 100, 2)
             sl_low_percent = round((diff_from_sl_low / dayLow) * 100, 2)
             
-            new_df.loc[len(new_df)] = [date, time_input, dayHigh, dayLow, isDayHighBreak, isDayLowBreak,isComesBackToDayHigh, isComesBackToDayLow, target_high,diff_from_target_high, target_high_percent, sl_high, diff_from_sl_high, sl_high_percent ,target_low, diff_from_target_low, target_low_percent, sl_low, diff_from_sl_low, sl_low_percent, closing, closing_for_high, closing_for_low, not (isDayHighBreak or isDayLowBreak)]
+            new_df.loc[len(new_df)] = [date, time_input, dayHigh, dayHighTime, dayLow, dayLowTime, isDayHighBreak, dayHighBreakTime, isDayLowBreak, dayLowBreakTime, isComesBackToDayHigh, isComesBackToDayLow, target_high, target_high_time, diff_from_target_high, target_high_percent, sl_high, sl_high_time, diff_from_sl_high, sl_high_percent ,target_low, tartget_low_time, diff_from_target_low, target_low_percent, sl_low, sl_low_time, diff_from_sl_low, sl_low_percent, closing, closing_for_high, closing_for_low, not (isDayHighBreak or isDayLowBreak)]
 
     columns_to_format = [
         'DayHigh', 'DayLow', 'TargetHigh', 'points_from_target_high', 'SLHigh', 'points_from_SL_High', 'TargetLow', 'points_from_Target_Low', 'SLLow', 'points_from_SL_low', 'Closing', 'ClosingPointDiffForHigh', 'ClosingPointDiffForLow'
@@ -202,10 +229,10 @@ def create_time_folders(destination_dir,source_dir):
         current_time += timedelta(minutes=interval_minutes)
 
 if __name__ == "__main__":
-    time_input = input("Enter time (HH:MM): ")
-    index = timeToIndex(time_input)
+    # time_input = input("Enter time (HH:MM): ")
+    # index = timeToIndex(time_input)
     source_dir = 'Refactored-Data'
     destination_folder = 'Backtesting'
     os.makedirs(destination_folder, exist_ok=True)
-    traverse_every_file(source_dir,destination_folder,index,time_input)
-    # create_time_folders(destination_folder,source_dir)
+    # traverse_every_file(source_dir,destination_folder,index,time_input)
+    create_time_folders(destination_folder,source_dir)
